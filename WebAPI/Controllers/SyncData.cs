9563c285b057c8cb9f4b0fc59ea23a487f015978
jagath99 <e19423@eng.pdn.ac.lk>
2025-07-28 14:58:09 +0530

    added api controllers checked with swagger

--- WebAPI/Controllers/SyncData.cs
+++ WebAPI/Controllers/SyncData.cs
@@ -1,6 +1,41 @@
-﻿namespace WebAPI.Controllers
+﻿using WebAPI.Models;
+using WebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
 {
-    public class SyncData
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ThirdPartyController : ControllerBase
     {
+        private readonly ThirdPartyAccess _thirdPartyAccess;
+
+        public ThirdPartyController(ThirdPartyAccess thirdPartyAccess)
+        {
+            _thirdPartyAccess = thirdPartyAccess;
+        }
+
+         
+        [HttpGet("post/{id}")]
+        public async Task<ActionResult<Post>> GetPost(int id)
+        {
+             var post = await _thirdPartyAccess.GetPostByIdAsync(id);
+             if (post == null)
+                 return NotFound();
+
+             return Ok(post);
+        }
+        [HttpGet("comment/{id}")]
+        public async Task<ActionResult<Comment>> GetComment(int id)
+        {
+              var comment = await _thirdPartyAccess.GetCommentByIdAsync(id);
+              if (comment == null)
+                    return NotFound();
+
+              return Ok(comment);
+        }
     }
 }
+
---
+++ WebAPI/Services/ThirdPartyAccess.cs
@@ -0,0 +1,20 @@
+﻿namespace WebAPI.Services;
+using System.Net.Http.Json;
+using WebAPI.Models;
+
+public class ThirdPartyAccess
+{
+    private readonly HttpClient _httpClient;
+    public ThirdPartyAccess(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<Post?> GetPostByIdAsync(int id)
+    {
+        return await _httpClient.GetFromJsonAsync<Post>($"http://jsonplaceholder.typicode.com/posts/{id}");
+    }
+    public async Task<Comment?> GetCommentByIdAsync(int id)
+    {
+        return await _httpClient.GetFromJsonAsync<Comment>($"http://jsonplaceholder.typicode.com/comments/{id}");
+    }
+}
