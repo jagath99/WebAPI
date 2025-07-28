f6f25a965da3cd75eeb59d18ef5f52460e51dc75
jagath99 <e19423@eng.pdn.ac.lk>
2025-07-28 13:14:38 +0530

    add Models and connected to sql

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.APIData;
+using WebAPI.Controllers;
+using WebAPI.Models;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<DataConnect>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
