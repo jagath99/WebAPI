643ae5342ea0ba818d1cb47109d8751a1e24998c
jagath99 <e19423@eng.pdn.ac.lk>
2025-07-28 16:45:38 +0530

    added excemoption handling . Remove warnings

--- WebAPI/Models/Comment.cs
+++ WebAPI/Models/Comment.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public int PostId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Body { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Body { get; set; }
     }
 }
